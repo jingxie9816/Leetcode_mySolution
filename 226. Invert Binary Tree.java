52a69386c7890b485677280af7c5c1c0f77ebfd0
Jing Xie <88932816+jingxie9816@users.noreply.github.com>
2023-04-28 12:13:51 -0700

    add solutions to 226

--- 226. Invert Binary Tree.java
+++ 226. Invert Binary Tree.java
@@ -1,3 +1,4 @@
+/*DFS(Recursion)*/
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
@@ -6,5 +7,28 @@ class Solution {
         root.left = right;
         root.right = left;
         return root;
+    }
+}
+
+
+/*BFS*/
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode curr = queue.poll();
+            TreeNode left = curr.left;
+            TreeNode right = curr.right;
+            curr.left = right;
+            curr.right = left;
+            if(curr.left != null) {
+                queue.offer(curr.left);
+                }
+            if (curr.right != null){
+                queue.offer(curr.right);
+            }
+        } return root;
     }
 }
