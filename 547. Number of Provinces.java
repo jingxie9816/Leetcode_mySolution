91a3d2bcabb8bab85e53b7ba59f6d7c47c10172e
Jing Xie <88932816+jingxie9816@users.noreply.github.com>
2023-06-22 18:12:00 -0700

    update 547

--- 547. Number of Provinces.java
+++ 547. Number of Provinces.java
@@ -1,3 +1,4 @@
+// dfs
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int provinces = 0;
@@ -20,5 +21,42 @@ class Solution {
                 dfs(j,cities,isConnected,visited);
             }
         }
+    }
+}
+
+// unionFind
+class Solution {
+    int[] parent;
+    public int findCircleNum(int[][] isConnected) {
+        int cities = isConnected.length;
+        parent = new int[cities];
+        int provinces = 0;
+        for (int i =0; i<cities; i++){
+            parent[i]=i;
+        }
+        for(int i = 0; i<cities; i++){
+            for (int j = 0; j<cities; j++){
+                if (isConnected[i][j]==1){
+                    union(i, j);
+                }
+            }
+        }
+        for (int i = 0; i<cities; i++){
+            if (parent[i] == i){
+                provinces++;
+            }
+        }
+    return provinces;
+    }
+
+    public void union(int i, int j){
+        parent[find(i)] = find(j); // let i's root becomes child of j's root
+    }
+
+    public int find(int i){ // find the root of i 
+        if (parent[i] != i){
+            parent[i] = find(parent[i]);
+        }
+        return parent[i];
     }
 }
